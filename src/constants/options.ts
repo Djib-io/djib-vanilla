1e48f6f58077c7c46efba330619b4495ae681d95
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-01 16:03:16 +0330

    add constants

---
+++ src/constants/options.ts
@@ -0,0 +1,31 @@
+import { Option } from "../components/Dropdown";
+
+export const networkOptions: Option[] = [
+  {
+    value: "mainnet",
+    label: "Mainnet",
+  },
+  {
+    value: "devnet",
+    label: "Devnet",
+  },
+  {
+    value: "testnet",
+    label: "Testnet",
+  },
+];
+
+export const walletMenu: Option[] = [
+  {
+    value: "copy",
+    label: "Copy address",
+  },
+  {
+    value: "change",
+    label: "Change wallet",
+  },
+  {
+    value: "disconnect",
+    label: "Disconnect",
+  },
+];
