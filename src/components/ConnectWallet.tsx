9744f8784440ae3f5c3e63d2436df10854606aab
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-01 12:29:17 +0330

    create 'ConnectWallet' component

---
+++ src/components/ConnectWallet.tsx
@@ -0,0 +1,88 @@
+import { useSpring, animated } from "@react-spring/web";
+import { WalletReadyState } from "@solana/wallet-adapter-base";
+import { useWallet, Wallet } from "@solana/wallet-adapter-react";
+import { WalletIcon } from "@solana/wallet-adapter-react-ui";
+import { useCallback, useEffect, useMemo } from "react";
+import styles from "./../styles/modules/ConnectWallet.module.scss";
+import Loading from "./Loading";
+
+function ConnectWallet() {
+  const { wallets, select, connecting } = useWallet();
+
+  const [_animatedStyles, api] = useSpring(() => ({
+    opacity: connecting ? 0 : 1,
+    scale: connecting ? 0.5 : 1,
+  }));
+
+  const [, , allWallets] = useMemo(() => {
+    const installed: Wallet[] = [];
+    const notDetected: Wallet[] = [];
+    const loadable: Wallet[] = [];
+
+    for (const wallet of wallets) {
+      if (wallet.readyState === WalletReadyState.NotDetected) {
+        notDetected.push(wallet);
+      } else if (wallet.readyState === WalletReadyState.Loadable) {
+        loadable.push(wallet);
+      } else if (wallet.readyState === WalletReadyState.Installed) {
+        installed.push(wallet);
+      }
+    }
+
+    return [
+      installed,
+      [...loadable, ...notDetected],
+      [...installed, ...loadable, ...notDetected],
+    ];
+  }, [wallets]);
+
+  useEffect(() => {
+    api.start({
+      opacity: connecting ? 0 : 1,
+      scale: connecting ? 0.5 : 1,
+    });
+  }, [api, connecting]);
+
+  const handleClick = useCallback(
+    (walletName) => {
+      console.log(walletName);
+      select(walletName);
+    },
+    [select]
+  );
+
+  return (
+    <div>
+      <animated.div style={_animatedStyles}>
+        <div className={styles.head}>
+          <p>Connect Wallet</p>
+          <div>Select your wallet</div>
+        </div>
+        <ul className={styles.wallets}>
+          {allWallets.map((wallet) => (
+            <li
+              key={`wallet-${wallet.adapter.name}`}
+              onClick={() => handleClick(wallet.adapter.name)}
+            >
+              <WalletIcon wallet={wallet} />
+              {wallet.adapter.name}
+            </li>
+          ))}
+        </ul>
+      </animated.div>
+      <animated.div
+        className={styles.loading}
+        style={{
+          opacity: _animatedStyles.opacity.to((value) => 1 - value),
+          transform: _animatedStyles.scale.to(
+            (value) => `scale(${1 - (value - 0.5)}) translate(-50%, -50%)`
+          ),
+        }}
+      >
+        <Loading />
+      </animated.div>
+    </div>
+  );
+}
+
+export default ConnectWallet;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,9 @@
+import Box from "../components/Box";
+import Boxes from "../components/Boxes";
+import ConnectWallet from "../components/ConnectWallet";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import BoxBrowser from "../providers/BoxBrowser";
 import "./../styles/App.css";
 
 function App() {
@@ -7,6 +11,11 @@ function App() {
     <div className="app">
       <Nav />
       <Header />
+      <BoxBrowser defaultPath='connect-wallet'>
+        <Boxes>
+          <Box path="connect-wallet" element={<ConnectWallet />} />
+        </Boxes>
+      </BoxBrowser>
     </div>
   );
 }
