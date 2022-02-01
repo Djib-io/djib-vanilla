1e23f1ad9084398d60b31a774cff7644d36a505d
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-01 16:08:54 +0330

    add options to 'WalletButton'

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
-import Button from './Button';
-import styles from './../styles/modules/WalletButton.module.scss'
-import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletIcon } from '@solana/wallet-adapter-react-ui';
+import React, { useState, useEffect, useCallback } from "react";
+import Button from "./Button";
+import styles from "./../styles/modules/WalletButton.module.scss";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletIcon } from "@solana/wallet-adapter-react-ui";
+import { useSpring, animated } from "@react-spring/web";
+import useMeasure from "react-use-measure";
+import { walletMenu } from "../constants/options";
 
 function WalletButton() {
+  const { publicKey, wallet, connected } = useWallet();
 
-    const { publicKey, wallet, connected } = useWallet()
+  const [show, setShow] = useState(false);
 
-    return (
-        <div>
-            <Button className={styles.button} light={connected} onClick={() => {}}>{publicKey ? (
-                <>
-                    <WalletIcon wallet={wallet} />
-                    <p>{publicKey.toString()}</p>
-                </>
-            ) : 'Connect Wallet'}</Button>
-        </div>
-    );
+  const [optionsRef, { height: viewHeight }] = useMeasure();
+  const { height, opacity } = useSpring({
+    from: { height: 0, opacity: 0 },
+    to: {
+      height: show ? viewHeight : 0,
+      opacity: show ? 1 : 0,
+    },
+    config: {
+      tension: 277,
+      friction: 24,
+      precision: 0.001,
+      velocity: 0.011,
+    },
+  });
+
+  useEffect(() => {
+    const handleWindowClick = () => {
+      setShow(false);
+    };
+    window.addEventListener("click", handleWindowClick);
+    return () => {
+      window.removeEventListener("click", handleWindowClick);
+    };
+  }, []);
+
+  const handleItemClick = useCallback((item: string) => {
+    setShow(false);
+  }, []);
+
+  const handleButtonClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      e.stopPropagation();
+      if (connected) setShow((prev) => !prev);
+    },
+    [connected]
+  );
+
+  return (
+    <div className={styles.container}>
+      <Button
+        className={styles.button}
+        light={connected}
+        onClick={handleButtonClick}
+      >
+        {publicKey ? (
+          <>
+            <WalletIcon wallet={wallet} />
+            <p>{publicKey.toString()}</p>
+          </>
+        ) : (
+          "Connect Wallet"
+        )}
+      </Button>
+      <animated.div style={{ height, opacity }} className={styles.options}>
+        <ul ref={optionsRef}>
+          {walletMenu.map((option) => (
+            <li
+              key={`option-item-${option.value}`}
+              onClick={() => handleItemClick((option as any).value)}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </animated.div>
+    </div>
+  );
 }
 
 export default WalletButton;
