edbdcd82b0364acbe16cf44d8199b89f8a9130c4
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-02 01:06:13 +0330

    create network context

---
+++ src/providers/NetworkProvider.tsx
@@ -0,0 +1,25 @@
+import React, {createContext, useContext, useState} from "react";
+import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
+
+
+const NetworkContext = createContext<WalletAdapterNetwork>(WalletAdapterNetwork.Mainnet)
+const NetworkDispatchContext = createContext<React.Dispatch<any>>(() => null)
+
+
+const NetworkProvider:React.FC = ({ children }) => {
+    const [state, setState] = useState(WalletAdapterNetwork.Mainnet)
+
+    return (
+        <NetworkDispatchContext.Provider value={setState}>
+            <NetworkContext.Provider value={state}>
+                {children}
+            </NetworkContext.Provider>
+        </NetworkDispatchContext.Provider>
+    );
+}
+
+export default NetworkProvider;
+
+
+export const useNetwork = () => useContext(NetworkContext)
+export const useNetworkDispatch = () => useContext(NetworkDispatchContext)
