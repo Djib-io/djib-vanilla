ae21113a5c956a8b85f57976e8eeda54619fdbab
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-06 18:28:15 +0330

    function for join url

---
+++ src/utils/urlJoiner.ts
@@ -0,0 +1,9 @@
+export const urlJoiner = (...args: string[]) =>
+    args
+        .join('/')
+        .replace(/[/]+/g, '/')
+        .replace(/^(.+):\//, '$1://')
+        .replace(/^file:/, 'file:/')
+        .replace(/\/(\?|&|#[^!])/g, '$1')
+        .replace(/\?/g, '&')
+        .replace('&', '?');
