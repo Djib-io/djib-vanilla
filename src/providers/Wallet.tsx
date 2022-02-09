b24300c2931fdd3686a1887f23aafdd37229f7c9
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-10 02:20:58 +0330

    clean code

--- src/providers/Wallet.tsx
+++ src/providers/Wallet.tsx
@@ -4,16 +4,14 @@ import {
     WalletProvider,
 } from "@solana/wallet-adapter-react";
 import {
-    // LedgerWalletAdapter,
     PhantomWalletAdapter,
     SlopeWalletAdapter,
     SolflareWalletAdapter,
     SolletExtensionWalletAdapter,
-    // TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import {WalletModalProvider} from "@solana/wallet-adapter-react-ui";
-import {clusterApiUrl} from "@solana/web3.js";
 import {useNetwork} from "./NetworkProvider";
+import {clusterApiUrl} from "@solana/web3.js";
 
 
 export const Wallet: FC = ({children}) => {
@@ -27,8 +25,6 @@ export const Wallet: FC = ({children}) => {
             new PhantomWalletAdapter(),
             new SlopeWalletAdapter(),
             new SolflareWalletAdapter(),
-            // new TorusWalletAdapter(),
-            // new LedgerWalletAdapter(),
             new SolletExtensionWalletAdapter({network}),
         ],
         [network]
