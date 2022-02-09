82007d9c8de11fe47867de2a8e8ba6c95a20d957
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-10 02:20:27 +0330

    add cluster api urls

--- src/providers/NetworkProvider.tsx
+++ src/providers/NetworkProvider.tsx
@@ -6,8 +6,19 @@ const NetworkContext = createContext<WalletAdapterNetwork>(WalletAdapterNetwork.
 const NetworkDispatchContext = createContext<React.Dispatch<any>>(() => null)
 
 
+export const clusterApiUrl = (network: WalletAdapterNetwork) => {
+    switch (network) {
+        case WalletAdapterNetwork.Mainnet:
+            return 'https://main.djib.io/'
+        case WalletAdapterNetwork.Devnet:
+            return 'https://dev.djib.io/'
+        case WalletAdapterNetwork.Testnet:
+            return 'https://test.djib.io/'
+    }
+}
+
 const NetworkProvider:React.FC = ({ children }) => {
-    const [state, setState] = useState(WalletAdapterNetwork.Mainnet)
+    const [state, setState] = useState(WalletAdapterNetwork.Devnet)
 
     return (
         <NetworkDispatchContext.Provider value={setState}>
