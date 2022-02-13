c0ca996d4a932eac32c7e8bd832d98297e60a75b
Amirhossein Sadeqi <sadeghi.ah79@gmail.com>
2022-02-13 20:00:19 +0330

    remove estimateReqConfig

--- src/api/configs/index.ts
+++ src/api/configs/index.ts
@@ -6,11 +6,6 @@ const api = apiConfigCreator()
 const jsonrpc = jsonRPC(api)
 
 
-export const estimateReqConfig = (size: number) => jsonrpc.config()
-    .method('estimate')
-    .params({size, unit: 'kb'})
-    .create()
-
 export const createPaymentReqConfig = (size: number, filenames: string[]) => jsonrpc.config()
     .method('createPayment')
     .params({size, unit: 'KB'}, filenames)
